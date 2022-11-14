b3c0540ee6c999004d103d054c7f7b4d103ac7ca
Felix Hennig <felix.hennig@stackable.tech>
2022-11-15 00:04:23 +0100

    a bit more cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,13 @@
 mod test_definition;
 mod test_generation;
-use anyhow::{Context, Ok, Result};
-use minijinja::{context, Environment, Source};
-use std::{
-    fs::DirEntry,
-    path::{Path, PathBuf},
-};
+use anyhow::{Ok, Result};
+use std::path::Path;
 use test_definition::TestDefinition;
 use test_generation::TestGeneration;
 
-// TODO we need to use walkdir
-// we can't build all the templates first, and then go through each scenario anymore
-// it would be better to go through each file and place it in all the scenarios immediately
-
-// walk dir
-// if directory:
-//   for every
-
 fn main() -> Result<()> {
     // read the test definition file
-
-    let file_contents = std::fs::read_to_string("test-definition.yaml")
-        .context("Failed to read test definition file")?;
-    let test_def: TestDefinition = serde_yaml::from_str(&file_contents).unwrap();
+    let test_def = TestDefinition::from_yaml_file(&Path::new("test-definition.yaml"))?;
     let test_gen =
         TestGeneration::new(&Path::new("tests"), &Path::new("templates/kuttl"), test_def);
     test_gen.generate()?;
--- src/test_definition.rs
+++ src/test_definition.rs
@@ -1,6 +1,7 @@
 //! Helper structs to read a YAML test definition
+use anyhow::{Context, Ok, Result};
 use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
+use std::{collections::HashMap, path::Path};
 
 #[derive(Serialize, Deserialize, PartialEq, Debug)]
 pub struct TestDefinition {
@@ -15,6 +16,13 @@ impl TestDefinition {
             hashmap.insert(dimension.name.clone(), dimension.values.clone());
         }
         hashmap
+    }
+
+    pub fn from_yaml_file(yaml_file: &Path) -> Result<TestDefinition> {
+        let file_contents =
+            std::fs::read_to_string(yaml_file).context("Failed to read test definition file")?;
+        let test_def: TestDefinition = serde_yaml::from_str(&file_contents).unwrap();
+        Ok(test_def)
     }
 }
 
